72d130c5ad174757b4d0dae10ebb5cf95c0ec26e
Dugit0 <dmitry.astrakhantsev@gmail.com>
2023-03-21 18:03:07 +0300

    Contest 3 Problem 3 4

--- contest03/4/4.cpp
+++ contest03/4/4.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
+#include "cmc_complex.h"
+#include "cmc_complex_stack.h"
+#include "cmc_complex_eval.h"
+
+
 
 using std::cin, std::cout, std::endl;
 
 
 
 int
-main(void)
+main(int argc, char *argv[])
 {
     
     return 0;
