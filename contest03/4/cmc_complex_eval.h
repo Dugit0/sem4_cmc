bb74211f6cefea7a11ef9d316a834d54c8978481
Dugit0 <dmitry.astrakhantsev@gmail.com>
2023-03-22 14:18:26 +0300

    Contest 3 Problem 4

---
+++ contest03/4/cmc_complex_eval.h
@@ -0,0 +1,8 @@
+//
+// Created by dmitry on 3/22/23.
+//
+
+#ifndef CODE_CMC_COMPLEX_EVAL_H
+#define CODE_CMC_COMPLEX_EVAL_H
+
+#endif //CODE_CMC_COMPLEX_EVAL_H
---
+++ contest03/4/cmc_complex_stack.h
@@ -0,0 +1,8 @@
+//
+// Created by dmitry on 3/22/23.
+//
+
+#ifndef CODE_CMC_COMPLEX_STACK_H
+#define CODE_CMC_COMPLEX_STACK_H
+
+#endif //CODE_CMC_COMPLEX_STACK_H
