b7e50f37685b0bba24038aafbeb2d73ca993aeb4
Dugit0 <dmitry.astrakhantsev@gmail.com>
2023-04-02 19:44:15 +0300

    Contest 4 Fin

--- contest04/4/4.cpp
+++ contest04/4/4.cpp
@@ -1,13 +1,65 @@
 #include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
+#include <cstdio>
+#include <cctype>
+#include <vector>
+#include <map>
+#include <functional>
+#include <new>
 
-using std::cin, std::cout, std::endl;
 
+class Mean
+{
+    double _sum;
+    unsigned int _count;
+public:
+    Mean(double sum = 0, unsigned int count = 0)
+    {
+        _sum = sum;
+        _count = count;
+    };
 
+    void
+    operator()(double n)
+    {
+        _sum += n;
+        _count++;
+    }
+
+    double
+    get_value() const
+    {
+        return _sum / _count;
+    }
+};
 
 int
 main(void)
 {
-    
+    std::vector<double> vec{};
+    double buf;
+    while (std::cin >> buf) {
+        vec.push_back(buf);
+    }
+    unsigned int n = vec.size();
+    unsigned int drop1 = n / 10;
+//    std::cout << drop1 << std::endl;
+    auto begin = vec.begin() + drop1;
+    auto end = vec.end() - drop1;
+    std::sort(begin, end);
+//    std::cout << end - begin << std::endl;
+    unsigned int drop2 = (end - begin) / 10;
+    begin += drop2;
+    end -= drop2;
+
+    Mean mean = std::for_each(begin, end, Mean());
+
+//    for (auto el = begin; el < end; el++) {
+//        std::cout << *el << " ";
+//    }
+    std::cout << std::fixed << std::setprecision(10) << mean.get_value() << std::endl;
     return 0;
 }
 
--- contest04/5/5.cpp
+++ contest04/5/5.cpp
@@ -1,13 +1,56 @@
 #include <iostream>
+#include <iomanip>
+#include <list>
+#include <iterator>
+#include <string>
+#include <cmath>
+#include <cstdio>
+#include <cctype>
+#include <vector>
+#include <map>
+#include <functional>
+#include <new>
 
-using std::cin, std::cout, std::endl;
 
 
 
 int
 main(void)
 {
-    
+    std::list<unsigned int> my_list{};
+    unsigned int buf;
+    while (std::cin >> buf) {
+        if (buf == 0) {
+            break;
+        }
+        my_list.push_back(buf);
+    }
+
+    int instr;
+    while (std::cin >> instr) {
+        if (instr < 0) {
+            instr = (-instr) - 1;
+            if ((unsigned int) instr < my_list.size()) {
+                auto rm_it = my_list.begin();
+                std::advance(rm_it, instr);
+                my_list.erase(rm_it);
+            }
+        } else if (instr > 0) {
+            unsigned int value;
+            std::cin >> value;
+            instr--;
+            if ((unsigned int) instr >= my_list.size()) {
+                my_list.push_back(value);
+            } else {
+                auto in_it = my_list.begin();
+                std::advance(in_it, instr);
+                my_list.insert(in_it, value);
+            }
+        }
+    }
+    for (auto el : my_list) {
+        std::cout << el << std::endl;
+    }
     return 0;
 }
 
