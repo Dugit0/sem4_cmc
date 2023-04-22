bf87b206d626ed1ee63458a752a9a74fc1518ccb
Dugit0 <dmitry.astrakhantsev@gmail.com>
2023-04-22 19:33:51 +0300

    Contest 6 FIN

--- contest06/5/5.cpp
+++ contest06/5/5.cpp
@@ -8,22 +8,41 @@
 #include <functional>
 #include <new>
 
-template<typename T>
-bool
-default_comp(const T &f, const T &s)
-{
-    return (f < s);
-}
+//template<typename T>
+//bool
+//default_comp(const T &f, const T &s)
+//{
+//    return (f < s);
+//}
 
+//bool
+//default_comp(const int &f, const int &s)
+//{
+//    return (f < s);
+//}
 
 template<typename Iter>
 void
 selection_sort(
-        Iter begin, Iter end,
+        Iter begin,
+        Iter end,
         std::function<bool(
                 const typename std::iterator_traits<Iter>::value_type &,
-                const typename std::iterator_traits<Iter>::value_type &)> comp)
+                const typename std::iterator_traits<Iter>::value_type &)>
+        comp = [](
+                const typename std::iterator_traits<Iter>::value_type &f,
+                const typename std::iterator_traits<Iter>::value_type &s) { return f < s; }
+)
 {
+//    for (auto i = begin; i != end; i++) {
+//        auto min = i;
+//        for (auto j = i; j != end; j++) {
+//            if (comp(*j, *min)) {
+//                min = j;
+//            }
+//        }
+//        std::swap(*i, *min);
+//    }
     auto i = begin;
     while (i != end) {
         auto j = i;
@@ -35,18 +54,19 @@ selection_sort(
             j++;
         }
         std::swap(*i, *min);
+        i++;
     }
 }
 
 
-int
-main(void)
-{
-    std::vector<int> a = { 2, 3, 4, 1, 7, 6, 5, 9, 8, 10 };
-    selection_sort(a.begin(), a.end(), default_comp);
-    for (auto el: a) {
-        std::cout << el << " ";
-    }
-    return 0;
-}
+//int
+//main(void)
+//{
+//    std::vector<int> a = { 2, 3, 4, 1, 7, 6, 5, 9, 8, 10 };
+//    selection_sort(a.begin(), a.end());
+//    for (auto el: a) {
+//        std::cout << el << " ";
+//    }
+//    return 0;
+//}
 
