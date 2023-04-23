819f3b9f490a2e518386f68d6d10897fc1fd43f4
Dugit0 <dmitry.astrakhantsev@gmail.com>
2023-04-23 16:59:32 +0300

    Contest 7 Problem 3, 5

--- contest07/3/3.cpp
+++ contest07/3/3.cpp
@@ -9,105 +9,162 @@
 #include <map>
 #include <functional>
 #include <new>
+#include <memory>
 
-class Figure
-{
-public:
-    virtual double
-    get_square() const
-    {
-        return 0;
-    }
-
-    virtual ~Figure()
-    {
-
-    }
-};
-
-class Rectangle : Figure
-{
-    double _x, _y;
-public:
-    Rectangle(double x = 0, double y = 0)
-    {
-        _x = x;
-        _y = y;
-    }
-    static Rectangle *
-    make(std::string s)
-    {
-        std::stringstream my_stream;
-        my_stream << s;
-        double x, y;
-        my_stream >> x;
-        my_stream >> y;
-        auto obj = new Rectangle(x, y);
-        return obj;
-    }
-    double
-    get_square() const override
-    {
-        return _x * _y;
-    }
-};
-
-class Square : Figure
-{
-    double _x;
-public:
-    Square(double x = 0)
-    {
-        _x = x;
-    }
-    static Square *
-    make(std::string s)
-    {
-        std::stringstream my_stream;
-        my_stream << s;
-        double x;
-        my_stream >> x;
-        auto obj = new Square(x);
-        return obj;
-    }
-    double
-    get_square() const override
-    {
-        return _x * _x;
-    }
-};
-
-class Circle : Figure
-{
-    double _r;
-public:
-    Circle(double r = 0)
-    {
-        _r = r;
-    }
-    static Circle *
-    make(std::string s)
-    {
-        auto obj = new Circle(std::stod(s));
-        return obj;
-    }
-    double
-    get_square() const override
-    {
-        return M_PI * _r * _r;
-    }
-};
-
-
-
-
-
-
-//int
-//main(void)
+//class Figure
 //{
-//    Circle s;
-//    auto n = s.make("10");
-//    std::cout << n->get_square() << std::endl;
-//    return 0;
-//}
+//public:
+//    virtual double
+//    get_square() const
+//    {
+//        return 0;
+//    }
+//
+//    virtual ~Figure()
+//    {
+//
+//    }
+//};
+//
+//class Rectangle : public Figure
+//{
+//    double _x, _y;
+//public:
+//    Rectangle(double x = 0, double y = 0)
+//    {
+//        _x = x;
+//        _y = y;
+//    }
+//
+//    static Rectangle *
+//    make(std::string s)
+//    {
+//        std::stringstream my_stream;
+//        my_stream << s;
+//        double x, y;
+//        my_stream >> x;
+//        my_stream >> y;
+//        auto obj = new Rectangle(x, y);
+//        return obj;
+//    }
+//
+//    double
+//    get_square() const override
+//    {
+//        return _x * _y;
+//    }
+//};
+//
+//class Square : public Figure
+//{
+//    double _x;
+//public:
+//    Square(double x = 0)
+//    {
+//        _x = x;
+//    }
+//
+//    static Square *
+//    make(std::string s)
+//    {
+//        std::stringstream my_stream;
+//        my_stream << s;
+//        double x;
+//        my_stream >> x;
+//        auto obj = new Square(x);
+//        return obj;
+//    }
+//
+//    double
+//    get_square() const override
+//    {
+//        return _x * _x;
+//    }
+//};
+//
+//class Circle : public Figure
+//{
+//    double _r;
+//public:
+//    Circle(double r = 0)
+//    {
+//        _r = r;
+//    }
+//
+//    static Circle *
+//    make(std::string s)
+//    {
+//        auto obj = new Circle(std::stod(s));
+//        return obj;
+//    }
+//
+//    double
+//    get_square() const override
+//    {
+//        return M_PI * _r * _r;
+//    }
+//};
+
+
+class Factory
+{
+public:
+    static Factory &
+    factory_instance()
+    {
+        static Factory instance;
+        return instance;
+    }
+
+    Factory()
+    {}
+
+    Factory(Factory const &) = delete;
+
+    void
+    operator=(Factory const &) = delete;
+
+    std::unique_ptr<Figure>
+    make_figure(std::string str)
+    {
+        std::map<std::string, std::function<std::unique_ptr<Figure>(std::string)>> oper = {
+                { "R", [](std::string params) {
+                    return std::unique_ptr<Figure>(Rectangle::make(params));
+                }},
+                { "S", [](std::string params) {
+                    return std::unique_ptr<Figure>(Square::make(params));
+                }},
+                { "C", [](std::string params) {
+                    return std::unique_ptr<Figure>(Circle::make(params));
+                }}
+        };
+        std::istringstream stream(str);
+        std::string type;
+        stream >> type;
+        std::string params;
+        std::getline(stream, params);
+        return oper[type](params);
+    }
+};
+
+bool
+my_cmp(const std::unique_ptr<Figure> &f, const std::unique_ptr<Figure> &s)
+{
+    return f->get_square() < s->get_square();
+}
+
+
+int
+main(void)
+{
+    std::string buf;
+    std::vector<std::unique_ptr<Figure>> vec = {};
+    while (std::getline(std::cin, buf)) {
+        vec.push_back(Factory::factory_instance().make_figure(buf));
+    }
+    std::stable_sort(vec.begin(), vec.end(), my_cmp);
+    for (auto el = vec.begin(); el != vec.end(); el++) {
+        std::cout << (*el)->to_string() << std::endl;
+    }
+    return 0;
+}
--- contest07/5/5.cpp
+++ contest07/5/5.cpp
@@ -7,13 +7,90 @@
 #include <map>
 #include <functional>
 #include <new>
+#include <cassert>
+
+// Solution based on
+// https://stackoverflow.com/questions/351845/finding-the-type-of-an-object-in-c
 
 
-
-int
-main(void)
+class Figure
 {
-    
-    return 0;
-}
+public:
+    virtual bool
+    equals(const Figure *fig) const
+    {
+        return false;
+    }
+
+    virtual ~Figure()
+    {
+
+    }
+};
+
+
+class Rectangle : public Figure
+{
+public:
+    int d1, d2;
+
+    Rectangle(int a, int b)
+    {
+        d1 = a;
+        d2 = b;
+    }
+
+    bool
+    equals(const Figure *fig) const override
+    {
+        auto res = dynamic_cast<const Rectangle *>(fig);
+        if (res) {
+//            std::cout << "res not null" << std::endl;
+            return (d1 == res->d1 && d2 == res->d2);
+        } else {
+//            std::cout << "res null" << std::endl;
+            return false;
+        }
+    }
+};
+
+
+class Triangle : public Figure
+{
+public:
+    int d1, d2, d3;
+
+    Triangle(int a, int b, int c)
+    {
+        d1 = a;
+        d2 = b;
+        d3 = c;
+    }
+
+    bool
+    equals(const Figure *fig) const override
+    {
+        auto res = dynamic_cast<const Triangle *>(fig);
+        if (res) {
+            return (d1 == res->d1 && d2 == res->d2 && d3 == res->d3);
+        } else {
+            return false;
+        }
+    }
+};
+
+//int
+//main(void)
+//{
+//    Rectangle r1(10, 5), r2(10, 2), r3(10, 5), r4(5, 10);
+//    Triangle t1(1, 2, 3);
+//    Figure *f = &r1;
+//
+//    assert(!f->equals(&r2));
+//    assert(f->equals(&r3));
+//    assert(!f->equals(&r4));
+//    assert(!f->equals(&t1));
+//    assert(!f->equals(nullptr));
+//    return 0;
+//}
 
