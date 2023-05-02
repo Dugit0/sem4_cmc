0d0d3dae638607d95f8d84ca6aee30098ae38bb4
Dugit0 <dmitry.astrakhantsev@gmail.com>
2023-05-03 01:22:39 +0300

    Contest 9 Problem 3

--- contest09/3/3.cpp
+++ contest09/3/3.cpp
@@ -9,11 +9,85 @@
 #include <new>
 
 
+// a^n 0^m b^n 1^m
+// n, m > 0
+
+// КЗ-грамматика
+// S -> LD
+// L -> aLb | ab
+// D -> ND1 | N1
+// bN -> Nb
+// aN -> a0
+// 0N -> 00
+
+int
+check(std::string str)
+{
+    auto i = str.begin();
+    int status = 1;
+    size_t a_counter = 0;
+    size_t b_counter = 0;
+    size_t null_counter = 0;
+    size_t ones_counter = 0;
+    while (i != str.end()) {
+        switch (status) {
+            case 1:
+                if (*i != 'a') {
+                    if (*i != '0') {
+                        return 0;
+                    } else {
+                        status++;
+                    }
+                } else {
+                    a_counter++;
+                    i++;
+                }
+                break;
+            case 2:
+                if (*i != '0') {
+                    if (*i != 'b') {
+                        return 0;
+                    } else {
+                        status++;
+                    }
+                } else {
+                    null_counter++;
+                    i++;
+                }
+                break;
+            case 3:
+                if (*i != 'b') {
+                    if (*i != '1') {
+                        return 0;
+                    } else {
+                        status++;
+                    }
+                } else {
+                    b_counter++;
+                    i++;
+                }
+                break;
+            case 4:
+                if (*i != '1') {
+                    return 0;
+                } else {
+                    ones_counter++;
+                    i++;
+                }
+                break;
+        }
+    }
+    return (a_counter == b_counter && null_counter == ones_counter);
+}
+
 
 int
 main(void)
 {
-    
+    std::string buf;
+    while (std::cin >> buf) {
+        std::cout << check(buf) << std::endl;
+    }
     return 0;
 }
 
--- contest09/4/4.cpp
+++ contest09/4/4.cpp
@@ -8,31 +8,56 @@
 #include <functional>
 #include <new>
 
+/*
+S -> aAd
+A -> aAd | bBc
+B -> bBc | eps
+*/
+
+void
+B(size_t cur, size_t k)
+{
+    if (cur + 2 <= k) {
+        std::cout << "b";
+        B(cur + 2, k);
+        std::cout << "c";
+    }
+}
+
+void
+A(size_t cur, size_t k)
+{
+    if (cur + 2 <= k - 2) {
+        std::cout << "a";
+        A(cur + 2, k);
+        std::cout << "d";
+        std::cout << "b";
+        B(cur + 2, k);
+        std::cout << "c";
+    } else if (cur + 2 <= k) {
+        std::cout << "b";
+        B(cur + 2, k);
+        std::cout << "c";
+    }
+}
+
+
+void
+S(size_t k)
+{
+    std::cout << "a";
+    A(2, k);
+    std::cout << "d" << std::endl;
+}
 
 
 int
 main(void)
 {
-    int k;
-    std::cin >> k;
-    if (k >= 4 && k % 2 == 0) {
-        for (int n = 1; n < k / 2; n++) {
-            for (int i = 0; i < n; i++) {
-                std::cout << "a";
-            }
-            for (int i = 0; i < k / 2 - n; i++) {
-                std::cout << "b";
-            }
-            for (int i = 0; i < k / 2 - n; i++) {
-                std::cout << "c";
-            }
-            for (int i = 0; i < n; i++) {
-                std::cout << "d";
-            }
-            std::cout << std::endl;
-        }
-    }
-    
+//    int k;
+//    std::cin >> k;
+    int k = 6;
+    S(k);
     return 0;
 }
 
