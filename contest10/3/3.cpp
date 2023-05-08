2c080ab4f200134fef07499e6d5e993ac35be12d
Dugit0 <dmitry.astrakhantsev@gmail.com>
2023-05-08 13:36:15 +0300

    Contest 10 Problem 3 begin

--- contest10/3/3.cpp
+++ contest10/3/3.cpp
@@ -13,7 +13,13 @@
 int
 main(void)
 {
-    
+    std::map<std::string, std::map<char, std::string>> rules = {};
+    std::string cur_buf, char_buf, new_buf;
+    std::cin >> cur_buf;
+    while (cur_buf != "END") {
+        std::cin >> char_buf >> new_buf;
+
+    }
     return 0;
 }
 
