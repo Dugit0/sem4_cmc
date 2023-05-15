2bcb8b7a2833f7255ad13f03f19b47c383d3ad61
Dugit0 <dmitry.astrakhantsev@gmail.com>
2023-05-15 03:45:45 +0300

    THE END

--- contest10/4/4_2.cpp
+++ contest10/4/4_2.cpp
@@ -83,48 +83,74 @@ to_polish(std::string str)
     return ans;
 }
 
+int
+my_max(int a, int b, int c)
+{
+    int max = std::max(a, b);
+    max = std::max(max, c);
+    return max;
+}
 
 std::string
 to_normal(std::vector<char> vec)
 {
     std::string ans = "";
-    std::stack<std::string> stc = {};
+//    std::stack<std::string> stc = {};
+    std::stack<std::pair<std::string, int>> stc = {};
     for (auto i = vec.begin(); i != vec.end(); i++) {
         if (std::islower(*i)) {
             std::string buf(1, *i);
-            stc.push(buf);
+            stc.push({ buf, 0 });
         } else {
-            std::string s2 = stc.top();
+            std::string s2 = stc.top().first;
+            int p2 = stc.top().second;
             stc.pop();
-            std::string s1 = stc.top();
+            std::string s1 = stc.top().first;
+            int p1 = stc.top().second;
             stc.pop();
             std::string buf(1, *i);
+            int p3 = get_prior(*i);
+            int prior = p3; // ???
+            if (p1 > p3) {
+                s1 = "(" + s1 + ")";
+            }
+            if (p2 >= p3) {
+                s2 = "(" + s2 + ")";
+            }
 //            buf = "(" + s1 + buf + s2 + ")";
             buf = s1 + buf + s2;
-            stc.push(buf);
+            stc.push({ buf, prior });
         }
     }
-    return stc.top();
+    return stc.top().first;
 }
-
-
 
 
 int
 main(void)
 {
-//    std::string str;
-//    std::cin >> str;
+    std::string str;
+    std::cin >> str;
 //    std::string str = "(a+b)/(c+d)";
 //    std::string str = "a+b*c";
-    std::string str = "(a+b)/(c+d+e+f)";
+//    std::string str = "(a+b)/(c+d+e+f)";
+//    std::string str = "a+(b*c)";
+//    std::vector<std::string> tests = {
+//            "(a+b)/(c+d)",
+//            "a+b*c",
+//            "(a+b)/(c+d+e+f)",
+//            "a+(b*c)",
+//            "((a+b)+(c+d))",
+//            "((a)+b)&c^d",
+//            "(((a)&b^c|((d))))",
+//            "a"
+//    };
+//    for (auto str : tests) {
     std::vector<char> pol = to_polish(str);
-    for (auto el: pol) {
-        std::cout << el << " ";
-    }
-    std::cout << std::endl;
     std::string ans = to_normal(pol);
     std::cout << ans << std::endl;
+
+//    }
 
     return 0;
 }
